2018d038a7849455830080e1a14cd2d5acf6c3d3
S-okubomy <okubo1504@gmail.com>
2023-08-15 15:20:45 +0900

    updated

--- nlp1.py
+++ nlp1.py
@@ -6,7 +6,6 @@ import datetime
 import numpy as np
 from numpy import linalg as lg
 from janome.tokenizer import Tokenizer
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 sys.setrecursionlimit(2000)
 
@@ -37,28 +36,26 @@ def get_dic(st_lst):
 
 
 def tmp1():
-    st_data = li().read("./st_data.txt").val.replace("？", "").replace("\n", "")
+    st_data = li().read("./st_data.txt").val
     print(f'st_data: {st_data}')
 
     #que = "確認はどうしたら良いですか？"
-    que = "どうしたら良いですか？".replace("？", "")
+    #que = "熱中症を防ぐにはどうしたら良いですか？"
+    que = "エアコンはなぜ必要ですか？"
 
     t = Tokenizer()
     st_lst = get_wakachi(t, st_data)
 
     dic = get_dic(st_lst)
-    print(dic)
-
-    vectorizer = TfidfVectorizer(max_df=0.8, token_pattern='(?u)\\b\\w+\\b')
-    d, m = get_np(st_lst, dic, vectorizer)
+    d, m = get_np(st_lst, dic)
 
     len_i, len_j = d.shape
-    print(len_i, len_j)
+    #print(len_i, len_j)
     d_map = {}
     for i in range(len_i):
-       d_map[i] = (d[i], m[i])
+        d_map[i] = (d[i], m[i])
 
-    sentence = get_sentence(dic,"", que, d_map, t, vectorizer)
+    sentence = get_sentence(dic,"", que, d_map, t)
     print(sentence)
 
 def get_wakachi(t, st_data):
@@ -68,21 +65,22 @@ def get_wakachi(t, st_data):
 def get_fixed_len_lst(arr, len_trg, ind_end):
     return arr[ind_end-len_trg if ind_end-len_trg >=0 else 0 : ind_end]
 
-def get_next_word(dic, prev_sentence, d_map, t, vectorizer):
+def get_next_word(dic, prev_sentence, d_map, t):
     len_w = len(d_map[0][0])
     print(prev_sentence)
     #cnv_arr = li(get_wakachi(t, prev_sentence + "")).map(lambda s: dic.get(s) if dic.get(s) != None else 0).filter(lambda s: s != 0).list
     #print(dic)
-    waka = " ".join(get_wakachi(t, prev_sentence))
+    waka = get_wakachi(t, prev_sentence)
+    cnv_arr = li(get_wakachi(t, prev_sentence + "")).map(lambda s: dic.get(s) if dic.get(s) != None else 0).list
+    #cnv_arr = li(waka).map(lambda s: dic.get(s)).list
+    #try:
+    #    #cnv_arr.remove(dic.get('.'))
+    #    #cnv_arr.remove(dic.get(' '))
+    #except ValueError:
+    #    pass
+    #cnv_arr = li(get_wakachi(t, prev_sentence + ".")).map(lambda s: dic.get(s)).list
 
-    cnv_arr = vectorizer.transform([waka]).toarray()
-
-    print(f'waka: {waka}')
-
-    print(f'cnv_arr: {cnv_arr[0]}')
-
-    # cnv_arr = li(get_wakachi(t, prev_sentence + "")).map(lambda s: dic.get(s) if dic.get(s) != None else 0).list
-
+    #if re.match("^.* $", prev_sentence.replace("\n", "")) != None:
     if re.match("^(.|\s)* $", prev_sentence) != None:
         cnv_arr.append(dic.get(' '))
 
@@ -92,13 +90,14 @@ def get_next_word(dic, prev_sentence, d_map, t, vectorizer):
     if re.match("^(.|\s)*?\n$", prev_sentence) != None:
         cnv_arr.append(dic.get('\n'))
 
+
+
     print(f'waka: {waka}')
     print(f'cnv: {cnv_arr}')
 
     ind_end = len(cnv_arr)
     #trg = np.array(zero_ume(get_fixed_len_lst(cnv_arr, len_w, ind_end), len_w))
-    #trg = np.array(get_fixed_len_lst(cnv_arr, len_w, ind_end))
-    trg = np.array(cnv_arr[0])
+    trg = np.array(get_fixed_len_lst(cnv_arr, len_w, ind_end))
     #print(trg)
     cos_sort_lst = get_cos_sort(trg, d_map)
 
@@ -112,7 +111,7 @@ def get_next_word(dic, prev_sentence, d_map, t, vectorizer):
    
     next_word = inv_dic(dic).get(next_val)
     print(f'{next_val} : {next_word}')
-    return next_val
+    return next_word
 
 
 def get_cos_sort(trg, d_map):
@@ -121,30 +120,37 @@ def get_cos_sort(trg, d_map):
     len_trg = len(trg)
     for (k,v) in d_map.items():
         len_d = len(v[0])
-        print(f'v  : {v[0]}')
-        print(f'trg: {trg}')
-        # if len_trg >= len(v[0]) or v[0][len_trg] == 0:
-        #     continue
+        #print(len_d-len_trg)
+        #org = zero_ume(v[0], len_trg)
+        #cos_val = cos_sim(trg, v[0][len_d-len_trg:])
+        if len_trg >= len(v[0]) or v[0][len_trg] == 0:
+            continue
 
-        cos_val = cos_sim(trg, v[0])
-        if cos_val > 0.0:
-            lst.append((v[1], cos_val, v[0]))
+        cos_val = cos_sim(trg, v[0][:len_trg])
+        #print(f'v: {v[0][len_d-len_trg:]} , val: {cos_val}')
+        #print(f'v: {v[0]} , val: {cos_val}')
         #lst.append((int(v[1]), cos_val))
+        #if not np.isnan(cos_val):
+        if cos_val > 0.0:
+            lst.append((int(v[0][len_trg]), cos_val, v[0][:len_trg]))
+        #lst.append((int(v[1]), cos_val))
+
 
     lst.sort(key = lambda x: x[1], reverse=True)
     return lst        
 
-def get_sentence(dic, sentence, cur_word, d_map, t, vectorizer):
+
+def get_sentence(dic, sentence, cur_word, d_map, t):
     print(f'cur_word: {cur_word}')
     if cur_word == None:
         return sentence + "。"
 
     sentence +=  cur_word
-    next_word = get_next_word(dic, sentence, d_map, t, vectorizer)
+    next_word = get_next_word(dic, sentence, d_map, t)
     if next_word == "end" or next_word == "XXXXXX" or len(sentence) > 500:
         return sentence + "。"
 
-    return get_sentence(dic, sentence, next_word, d_map, t, vectorizer)
+    return get_sentence(dic, sentence, next_word, d_map, t)
 
 def cos_sim(v1, v2):
     return v1 @ v2 / (lg.norm(v1) * lg.norm(v2))
@@ -152,10 +158,7 @@ def cos_sim(v1, v2):
 def div_data(words):
     #lds = words[0:-1]
     #mds = words[-1]
-    #print(words)
-    len_trg = len(words)
-    #print(len_trg)
-    return (" ".join(words[:len_trg-1]), "".join(words[len_trg-1]))
+    return (words, 0)
 
 def zero_ume(arr, len_w):
     arr.extend([0 for _ in range(len(arr),len_w)])
@@ -165,54 +168,13 @@ def inv_dic(dic):
     return { v:k for k,v in dic.items() } 
 
 
-def get_np(st_lst, dic, vectorizer):
-    cnv_lst = st_lst
-    print(st_lst)
-
-    len_w = 20  # 特徴量の配列長さ
-    docs = []
-    ms = []
-    len_date = len(cnv_lst)
-    for end in range(2,len_date+len_w):
-        d, m = div_data(get_fixed_len_lst(cnv_lst, len_w, end))
-        if len(d) == 0:
-            continue
-        docs.append(d)
-        ms.append(m)
-    
-    print(docs)
-
-    vals = vectorizer.fit_transform(docs).toarray()
-    words = vectorizer.get_feature_names_out()
-
-    print(words)
-    print(vals)
-    print(ms)
-
-    len_ws = len(words)
-
-    d_arr = np.empty((0,len_ws), dtype=int)
-    m_arr = np.array([])
-    len_ms = len(ms)
-    for i in range(len_date):
-        dn = np.array([vals[i]])
-        d_arr = np.append(d_arr, dn, axis=0)
-        m_arr = np.append(m_arr,ms[i])
-
-    return (d_arr, m_arr)
-
-
-
-def X_get_np(st_lst, dic):
+def get_np(st_lst, dic):
     cnv_lst = li(st_lst).map(lambda s : dic.get(s)).list
     print(cnv_lst)
 
     len_w = 50  # 特徴量の配列長さ
     d_arr = np.empty((0,len_w), dtype=int)
     m_arr = np.array([])
-
-    vectorizer = TfidfVectorizer(smooth_idf = False)
-
     len_date = len(cnv_lst)
     for end in range(2,len_date+len_w):
         d, m = div_data(get_fixed_len_lst(cnv_lst, len_w, end))
